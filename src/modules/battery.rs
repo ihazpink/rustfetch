de05b4bdaef9b00d345a0b0722a3d31a66bc955d
ihazpink <ihazpink@gmail.com>
2021-10-21 17:35:54 +0200

    Changed n to '\n' because it is only a single character

--- src/modules/battery.rs
+++ src/modules/battery.rs
@@ -6,6 +6,6 @@ use std::fs;
 /// Get formatted battery info.
 pub fn get_battery() -> String {
     let battery_perc = fs::read_to_string("/sys/class/power_supply/BAT0/capacity").unwrap();
-    let battery_perc = battery_perc.trim_end_matches("\n");
+    let battery_perc = battery_perc.trim_end_matches('\n');
     format!("{}: {}%", "Battery".bright_blue(), battery_perc)
 }
